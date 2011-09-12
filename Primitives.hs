ccf54c0de7c8c8568768768f2fee64307dc3fb46
Andy Kitchen <kitchen.andy+git@gmail.com>
2011-09-12 15:54:57 +1000

    Added more primitives.

--- Primitives.hs
+++ Primitives.hs
@@ -22,16 +22,26 @@ defaultBindings = sequence
                      (">=", compLift (>=)),
 
 
-                     ("print", PrimFun putStrPrim)]
+                     ("print", PrimFun putStrPrim),
+                     ("read",  PrimFun getLinePrim)]
                 ]
 
+type Number = Double
+
+arithLift :: (Number -> Number -> Number) -> Value
 arithLift f =
   PrimFun (\[(NumVal x),(NumVal y)] -> (return . NumVal) (f x y))
 
 compLift :: (Value -> Value -> Bool) -> Value
-compLift comp = PrimFun (\[a, b] -> (return . BoolVal) (a `comp` b))
+compLift comp =
+  PrimFun (\[a, b] -> (return . BoolVal) (a `comp` b))
 
 putStrPrim :: [Value] -> ProgramEnv Value
 putStrPrim [(StrVal str)] = do
            (liftIO . putStr) str
            return Undefined
+
+getLinePrim :: [Value] -> ProgramEnv Value
+getLinePrim [] = do
+           line <- liftIO getLine
+           return (StrVal line)
