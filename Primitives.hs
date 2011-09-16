636076a1369526c2bcbc48359ee61357cf49a38f
Andy Kitchen <kitchen.andy+git@gmail.com>
2011-09-16 13:01:09 +1000

    Added a string conversion primitive.

--- Primitives.hs
+++ Primitives.hs
@@ -24,6 +24,7 @@ defaultBindings = sequence
 
                      ("!", logicLift1 (not)),
 
+                     ("string", PrimFun showPrim),
 
                      ("print", PrimFun putStrPrim),
                      ("read",  PrimFun getLinePrim)]
@@ -47,8 +48,17 @@ putStrPrim :: [Value] -> ProgramEnv Value
 putStrPrim [(StrVal str)] = do
            (liftIO . putStr) str
            return Undefined
+putStrPrim [(NumVal  num)]  = print' num
+putStrPrim [(BoolVal bool)] = print' bool
+
+print' val = (liftIO . print) val >> return Undefined
 
 getLinePrim :: [Value] -> ProgramEnv Value
 getLinePrim [] = do
            line <- liftIO getLine
            return (StrVal line)
+
+showPrim :: [Value] -> ProgramEnv Value
+showPrim [(NumVal  v)] = (return . StrVal . show) v
+showPrim [(BoolVal v)] = (return . StrVal . show) v
+showPrim _ = return Undefined
