43c183421caad54fadd997c474811d206bad10ad
Andy Kitchen <kitchen.andy+git@gmail.com>
2011-10-02 18:39:34 +1100

    Small cleanup.

--- Primitives.hs
+++ Primitives.hs
@@ -49,14 +49,14 @@ showPrim    :: Prim
 shiftPrim   :: Prim
 resetPrim   :: Prim
 
-plusPrim [(NumVal x),(NumVal y)] = (return . NumVal) (x +  y)
-plusPrim [(StrVal x),(StrVal y)] = (return . StrVal) (x ++ y)
+plusPrim [NumVal x, NumVal y] = (return . NumVal) (x +  y)
+plusPrim [StrVal x, StrVal y] = (return . StrVal) (x ++ y)
 
-putStrPrim [(StrVal str)] = do
+putStrPrim [StrVal str] = do
            (liftIO . putStr) str
            return Undefined
-putStrPrim [(NumVal  num)]  = print' num
-putStrPrim [(BoolVal bool)] = print' bool
+putStrPrim [NumVal  num]  = print' num
+putStrPrim [BoolVal bool] = print' bool
 
 print' val = (liftIO . print) val >> return Undefined
 
@@ -64,8 +64,8 @@ getLinePrim [] = do
            line <- liftIO getLine
            return (StrVal line)
 
-showPrim [(NumVal  v)] = (return . StrVal . show) v
-showPrim [(BoolVal v)] = (return . StrVal . show) v
+showPrim [NumVal  v] = (return . StrVal . show) v
+showPrim [BoolVal v] = (return . StrVal . show) v
 showPrim _ = return Undefined
 
 unsafeCoercePrompt :: Prompt ans a -> Prompt ansx a
@@ -75,21 +75,15 @@ unsafeCoercePrim = unsafeCoerce
 
 shiftPrim [PromptVal p, f@(Fun _ _ _)] =
             shift (unsafeCoercePrompt p) $ \k -> 
-              let k' = (\[val] -> k (return val)) in
+              let k' [val] = k (return val) in
               apply f [PrimFun (unsafeCoercePrim k')]
--- shiftPrim = undefined
 
 resetPrim [f@(Fun _ _ _)] =
-          reset (\p -> (apply f [(PromptVal (unsafeCoercePrompt p))]))
--- resetPrim = undefined
--- resetPrim [f@(Fun _ _ _)] = reset (resetHandler f)
--- resetHandler :: Value -> Prompt r a -> ProgramEnv Value
--- resetHandler f p = apply f [(PromptVal p)]
-
+          reset $ \p -> apply f [PromptVal (unsafeCoercePrompt p)]
 
 arithLift :: (Number -> Number -> Number) -> Value
 arithLift f =
-  PrimFun (\[(NumVal x),(NumVal y)] -> (return . NumVal) (f x y))
+  PrimFun (\[NumVal x, NumVal y] -> (return . NumVal) (f x y))
 
 compLift :: (Value -> Value -> Bool) -> Value
 compLift comp =
@@ -97,9 +91,9 @@ compLift comp =
 
 logicLift1 :: (Bool -> Bool) -> Value
 logicLift1 op =
-  PrimFun (\[(BoolVal a)] -> (return . BoolVal) (op a))
+  PrimFun (\[BoolVal a] -> (return . BoolVal) (op a))
 
 logicLift :: (Bool -> Bool -> Bool) -> Value
 logicLift op =
-  PrimFun (\[(BoolVal a), (BoolVal b)] -> (return . BoolVal) (a `op` b))
+  PrimFun (\[BoolVal a, BoolVal b] -> (return . BoolVal) (a `op` b))
 
